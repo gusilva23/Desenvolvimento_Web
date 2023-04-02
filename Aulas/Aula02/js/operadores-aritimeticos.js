823d5ffcbe2f2c5968e4342912c7df290377f624
gusilva23 <gusilva_21@outlook.com>
2023-04-02 12:25:39 -0300

    Adicionando aula 02 | Operadores JS e primeira página HTML

---
+++ Aulas/Aula02/js/operadores-aritimeticos.js
@@ -0,0 +1,28 @@
+var numero1 = 3
+var numero2 = 2
+
+var salarioAtual = 1000
+var reajusteSalarial = 10
+var valorReajuste = salarioAtual * reajusteSalarial / 100
+var salarioFinal = salarioAtual + valorReajuste
+
+console.log("--- Adição ---")
+console.log(numero1, " + ", numero2, " = ", numero1 + numero2)
+
+console.log("--- Subtração ---")
+console.log(numero1, " - ", numero2, " = ", numero1 - numero2)
+
+console.log("--- Multiplicação ---")
+console.log(numero1, " x ", numero2, " = ", numero1 * numero2)
+
+console.log("--- Divisão ---")
+console.log(numero1, " / ", numero2, " = ", numero1 / numero2)
+
+console.log("--- Potenciação ---")
+console.log(numero1, " ** ", numero2, " = ", numero1 ** numero2)
+
+console.log("--- Resto da divisão ---")
+console.log(numero1, " % ", numero2, " = ", numero1 % numero2)
+
+console.log("--- Porcentagem ---")
+console.log(salarioAtual, " + ", salarioAtual * reajusteSalarial / 100)
---
+++ Aulas/Aula02/js/operadores-atribuição.js
@@ -0,0 +1,7 @@
+var numero = 5
+
+numero = numero + 1
+numero += 1
+numero -= 1
+numero *= 1
+numero /= 1
