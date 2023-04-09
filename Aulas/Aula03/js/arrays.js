ccbc29ac318f595910ca67634ba554cc83eda5a1
gusilva23 <gusilva_21@outlook.com>
2023-04-09 12:15:44 -0300

    adicionando aula 3

---
+++ Aulas/Aula03/js/arrays.js
@@ -0,0 +1,19 @@
+var listaNomes = ["Fulano", "Ciclano", "Beltrano"]
+console.log(listaNomes) // imprimir valores
+
+listaNomes.push("Erculano")
+console.log(listaNomes) //imprimir valores
+
+listaNomes.pop()
+console.log(listaNomes) // remove o ultimo item da lista
+
+listaNomes.shift()
+console.log(listaNomes) // remove o primeiro item da lista
+
+listaNomes.sort()
+console.log(listaNomes) // ordena lista em ordem alfabética
+
+listaNomes.reverse()
+console.log(listaNomes) // inverte a ordem dos itens da lista
+
+console.log("Tamanho da lista", listaNomes.length)
